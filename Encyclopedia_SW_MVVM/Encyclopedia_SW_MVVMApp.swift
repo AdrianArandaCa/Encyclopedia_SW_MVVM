40017b8d147552f3074f2412cb22a0a6eab70d27
Adrian Aranda <aacampanario91@gmail.com>
2023-11-26 09:39:38 +0100

    Initial Commit

---
+++ Encyclopedia_SW_MVVM/Encyclopedia_SW_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Encyclopedia_SW_MVVMApp.swift
+//  Encyclopedia_SW_MVVM
+//
+//  Created by Dev on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Encyclopedia_SW_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
